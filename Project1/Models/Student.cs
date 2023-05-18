228ef9c6d7cea7c4180add6663b04400455ab115
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-18 12:03:19 +0530

    Add files via upload

---
+++ Project1/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace project1.Models
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }   
+        public string address { get; set; }
+    }
+}
---
+++ Project1/Service/StudentDetails.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using project1.Models;
+
+namespace project1.Service
+{
+    internal class StudentDetails
+    {
+        List<Student> list = new List<Student>();
+        public int CreateStudent(Student student)
+        {
+            list.Add(student);
+            return 1;
+        }
+        public List<Student> GetList()
+        {
+            return list;
+        }
+
+        public void update(int id)
+        {
+            List<Student> students = GetList();
+            try
+            {
+                foreach(Student stu in students)
+                {
+                    if (stu.Id == id)
+                    {
+                        stu.Id = id;
+                        Console.WriteLine("enter the new name");
+                        stu.Name = Console.ReadLine();
+                        Console.WriteLine("enter the new class");
+                        stu.address = Console.ReadLine();
+                    }
+                }
+            }catch (Exception ex) { ex.ToString(); }
+        }
+
+        public void delete(int id)
+        {
+            List<Student> students = GetList();
+            try
+            {
+                foreach(Student stu in students)
+                {
+                    if(stu.Id == id)
+                    {
+                        students.Remove(stu);
+                    }
+                }
+            }
+            catch (Exception ex) { ex.ToString(); }
+        }
+        public void search(int id)
+        {
+            Student st = new Student();
+            List<Student> students = GetList();
+            try
+            {
+                foreach (Student stu in students)
+                {
+                    if (stu.Id == id)
+                    {
+                        Console.WriteLine("name : " + stu.Name);
+                        Console.WriteLine("class name: " + stu.address);
+                        break;
+                    }
+
+                }
+            }
+            catch (Exception ex) { ex.ToString(); }
+        }
+    }
+}
+    
