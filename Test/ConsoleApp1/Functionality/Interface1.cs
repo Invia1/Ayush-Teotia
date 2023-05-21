f217a3dcb3d47d086983777ff375c352c8957f83
Invia1 <mayankgupta0507@gmail.com>
2023-05-21 21:14:10 +0530

    new test project

---
+++ Test/ConsoleApp1/Functionality/Interface1.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Functionality
+{
+    internal interface Interface
+    {
+        int addPatient(Patient temp);
+        List<Patient> display();
+
+    }
+}
---
+++ Test/ConsoleApp1/Models/Patient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    internal class Patient
+    {
+        public int PatientRegistrationId { get; set; }
+        public string PatientFirstName { get; set; }
+        public string PatientLastName { get; set; }
+        public double PatientPhoneNo { get; set; }
+        public string PatientGender { get; set; }
+        public int PatientAge { get; set; }
+        public string PatientAddress { get; set; }
+        public double PatientAdharNo { get; set; }
+
+    }
+}
